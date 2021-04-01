de98c19120b949625c71c22dd2aa4c7776eaac0a
Ante Kegalj <akegalj@gmail.com>
2021-04-01 13:34:01 +0200

    Refactor and cleanup

--- src/api/heap_profiler.rs
+++ src/api/heap_profiler.rs
@@ -12,10 +12,6 @@ type Size = u32;
 // profiler state can be implemented in wasm if overhead
 // of invoking host functions will be too big.
 pub struct HeapProfilerState {
-    malloc_counter: u32,
-    calloc_counter: u32,
-    realloc_counter: u32,
-    free_counter: u32,
     memory: HashMap<Ptr, Size>,
     live_heap_size: u64,
     total_allocated: u64,
@@ -27,59 +23,48 @@ impl StateMarker for HeapProfilerState {}
 impl HeapProfilerState {
     pub fn new() -> Self {
         Self {
-            malloc_counter: 0,
-            calloc_counter: 0,
-            realloc_counter: 0,
-            free_counter: 0,
             memory: HashMap::new(),
             live_heap_size: 0,
             total_allocated: 0,
-            heap_history: vec![],
+            heap_history: vec![(0, SystemTime::now())],
         }
     }
 }
 
 #[host_functions(namespace = "heap_profiler")]
 impl HeapProfilerState {
-    // TODO: check if calloc/realloc are implemented through malloc/free
     fn malloc_profiler(&mut self, size: Size, ptr: Ptr) {
-        debug!("{} malloc({}) -> {}", self.malloc_counter, size, ptr);
-        self.malloc_counter += 1;
+        debug!("heap_profiler: malloc({}) -> {}", size, ptr);
         self.memory.insert(ptr, size);
         self.total_allocated += size as u64;
         self.live_heap_size += size as u64;
         self.heap_history
             .push((self.live_heap_size, SystemTime::now()));
         debug!(
-            "live_heap={} allocated={}",
+            "heap_profiler: live_heap={} allocated={}",
             self.live_heap_size, self.total_allocated
         );
     }
 
     fn calloc_profiler(&mut self, len: Size, elem_size: Size, ptr: Ptr) {
-        debug!(
-            "{} calloc({},{}) -> {}",
-            self.calloc_counter, len, elem_size, ptr
-        );
+        debug!("heap_profiler: calloc({},{}) -> {}", len, elem_size, ptr);
         let size = len * elem_size;
-        self.calloc_counter += 1;
         self.memory.insert(ptr, size);
         self.total_allocated += size as u64;
         self.live_heap_size += size as u64;
         self.heap_history
             .push((self.live_heap_size, SystemTime::now()));
         debug!(
-            "live_heap={} allocated={}",
+            "heap_profiler: live_heap={} allocated={}",
             self.live_heap_size, self.total_allocated
         );
     }
 
     fn realloc_profiler(&mut self, old_ptr: Ptr, size: Size, new_ptr: Ptr) {
         debug!(
-            "{} realloc({},{}) -> {}",
-            self.realloc_counter, old_ptr, size, new_ptr
+            "heap_profiler: realloc({},{}) -> {}",
+            old_ptr, size, new_ptr
         );
-        self.realloc_counter += 1;
         // TODO: log error/trap if unwrap fails
         let removed_size = self.memory.remove(&old_ptr).unwrap();
         self.memory.insert(new_ptr, size);
@@ -89,14 +74,13 @@ impl HeapProfilerState {
         self.heap_history
             .push((self.live_heap_size, SystemTime::now()));
         debug!(
-            "live_heap={} allocated={}",
+            "heap_profiler: live_heap={} allocated={}",
             self.live_heap_size, self.total_allocated
         );
     }
 
     fn free_profiler(&mut self, ptr: Ptr) {
-        debug!("{}. free({})", self.free_counter, ptr);
-        self.free_counter += 1;
+        debug!("heap_profiler: free({})", ptr);
         if ptr != 0 {
             // TODO: log error/trap if unwrap fails
             let size = self.memory.remove(&ptr).unwrap();
@@ -105,7 +89,7 @@ impl HeapProfilerState {
                 .push((self.live_heap_size, SystemTime::now()));
         }
         debug!(
-            "live_heap={} allocated={}",
+            "heap_profiler: live_heap={} allocated={}",
             self.live_heap_size, self.total_allocated
         );
     }
--- src/module/normalisation/heap_profiler.rs
+++ src/module/normalisation/heap_profiler.rs
@@ -6,76 +6,70 @@ pub fn patch(module: &mut Module) -> Result<()> {
     add_profiler_to(module, "malloc")?;
     add_profiler_to(module, "calloc")?;
     add_profiler_to(module, "realloc")?;
-    add_profiler_to(module, "free")?;
-
-    Ok(())
+    add_profiler_to(module, "free")
 }
 
 fn add_profiler_to(module: &mut Module, name: &str) -> Result<()> {
-    let target_function_id = module
+    let function_id = module
         .funcs
         .by_name(name)
         .ok_or(anyhow::Error::msg(format!(
             "heap_profiler: '{}' was not found in wasm",
             name
         )))?;
-    let target_function = module
+    let function = &module
         .funcs
         .iter_local()
-        .filter(|(id, _)| id == &target_function_id)
+        .filter(|(id, _)| id == &function_id)
         .next()
         .unwrap()
         .1;
-    let args_len = target_function.args.len();
-    let rets_len = module.types.results(target_function.ty()).len();
 
-    // we assume args and returns are I32
+    let function_args = function.args.clone();
+    let arg_types = module.types.params(function.ty()).to_vec();
+    let ret_types = module.types.results(function.ty()).to_vec();
+
     let profiler_type = module
         .types
-        .add(&vec![ValType::I32; args_len + rets_len], &[]);
+        .add(&[arg_types.clone(), ret_types.clone()].concat(), &[]);
     let (profiler, _) = module.add_import_func(
         "heap_profiler",
         &format!("{}_profiler", name),
         profiler_type,
     );
-    // we asume args are I32 type
-    let local_vars: Vec<LocalId> = (0..args_len)
-        .map(|_| module.locals.add(ValType::I32))
-        .collect();
 
-    let target_function = module
+    let function_body = &mut module
         .funcs
         .iter_local_mut()
-        .filter(|(id, _)| id == &target_function_id)
+        .filter(|(id, _)| id == &function_id)
         .next()
         .unwrap()
-        .1;
+        .1
+        .builder_mut()
+        .func_body();
+
+    let locs = &mut module.locals;
+    let local_vars: Vec<LocalId> = arg_types.iter().map(|t| locs.add(*t)).collect();
 
     // save function args to local var
     local_vars
         .iter()
-        .zip(0..)
-        .for_each(|(local_var, func_ind)| {
-            let func_arg = target_function.args[func_ind];
-            target_function
-                .builder_mut()
-                .func_body()
+        .zip(function_args)
+        .for_each(|(local_var, func_arg)| {
+            function_body
                 .local_get_at(0, func_arg)
                 .local_set_at(1, *local_var);
         });
-    let return_val = match rets_len {
-        0 => None,
-        _ => {
-            // we assume return is I32 type
+    let return_val = match ret_types[..] {
+        [t] => {
             // we assume there is only one return
-            Some(module.locals.add(ValType::I32))
+            Some(module.locals.add(t))
         }
+        _ => None,
     };
 
     // find return instruction indexes
-    let return_indexes: Vec<usize> = target_function
-        .builder_mut()
-        .func_body()
+    let return_indexes: Vec<usize> = function_body
         .instrs()
         .iter()
         .enumerate()
@@ -86,36 +80,26 @@ fn add_profiler_to(module: &mut Module, name: &str) -> Result<()> {
         .map(|(i, _)| i)
         .collect();
 
-    // insert malloc profiler at the specific position
-    let end_index = target_function.builder_mut().func_body().instrs().len();
+    let end_index = function_body.instrs().len();
     // Insert profiler function call at specific position
     let mut insert_profiler_at = |pos: usize| {
-        let mut body = target_function.builder_mut().func_body();
-        //fn insert_local_args(pos: usize, body: &mut InstrSeqBuilder, local_vars: Vec<LocalId>) {
-        //    local_vars.iter().rev().for_each(|var| {
-        //        body.local_get_at(pos, *var);
-        //    })
-        //}
+        let insert_local_args = |body: &mut InstrSeqBuilder| {
+            local_vars.iter().rev().for_each(|var| {
+                body.local_get_at(pos, *var);
+            })
+        };
         match return_val {
             None => {
-                body.call_at(pos, profiler);
-                local_vars.iter().rev().for_each(|var| {
-                    body.local_get_at(pos, *var);
-                })
+                function_body.call_at(pos, profiler);
+                insert_local_args(function_body);
             }
             Some(ret_val) => {
-                body.call_at(pos, profiler);
-                body.local_get_at(pos, ret_val);
-                local_vars.iter().rev().for_each(|var| {
-                    body.local_get_at(pos, *var);
-                });
-                body.local_tee_at(pos, ret_val);
+                function_body.call_at(pos, profiler);
+                function_body.local_get_at(pos, ret_val);
+                insert_local_args(function_body);
+                function_body.local_tee_at(pos, ret_val);
             }
         }
-        //.call_at(i, profiler);
-        //.local_get_at(i, my_malloc_ret)
-        //.local_get_at(i, my_malloc_args)
-        //.local_tee_at(i, my_malloc_ret);
     };
 
     // call profiler at the end of the function
