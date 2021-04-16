d9fdab2b82582e7507b0ce54b28bd3817ec2f3f5
Ante Kegalj <akegalj@gmail.com>
2021-04-16 15:05:06 +0200

    Refactor profiler normalisation

--- src/module/normalisation/heap_profiler.rs
+++ src/module/normalisation/heap_profiler.rs
@@ -1,113 +1,114 @@
+use log::debug;
 use walrus::*;
 
 /// FIXME: add docs
 /// FIXME: enable this patch only with runtime flag
 pub fn patch(module: &mut Module) -> Result<()> {
     add_profiler_to(module, "malloc")?;
-    add_profiler_to(module, "calloc")?;
-    add_profiler_to(module, "realloc")?;
-    add_profiler_to(module, "free")
+    //    add_profiler_to(module, "calloc")?;
+    //    add_profiler_to(module, "realloc")?;
+    add_profiler_to(module, "free")?;
+    Ok(())
 }
 
 fn add_profiler_to(module: &mut Module, name: &str) -> Result<()> {
-    let function_id = module
+    let fn_id = module
         .funcs
         .by_name(name)
         .ok_or(anyhow::Error::msg(format!(
             "heap_profiler: '{}' was not found in wasm",
             name
         )))?;
-    let function = &module
-        .funcs
-        .iter_local()
-        .filter(|(id, _)| id == &function_id)
-        .next()
-        .unwrap()
-        .1;
+    let types = module.types.params_results(module.funcs.get(fn_id).ty());
+    let (params, results) = (types.0.to_vec(), types.1.to_vec());
 
-    let function_args = function.args.clone();
-    let arg_types = module.types.params(function.ty()).to_vec();
-    let ret_types = module.types.results(function.ty()).to_vec();
-
+    // profilers don't return anything
     let profiler_type = module
         .types
-        .add(&[arg_types.clone(), ret_types.clone()].concat(), &[]);
-    let (profiler, _) = module.add_import_func(
-        "heap_profiler",
-        &format!("{}_profiler", name),
-        profiler_type,
+        .add(&[params.clone(), results.clone()].concat(), &[]);
+    let profiler_id = module
+        .add_import_func(
+            "heap_profiler",
+            &format!("{}_profiler", name),
+            profiler_type,
+        )
+        .0;
+
+    let mut fn_builder = FunctionBuilder::new(&mut module.types, &params, &results);
+    let fn_local_function = module.funcs.get(fn_id).kind.unwrap_local();
+    fn_builder.name(format!("{}_wrap", name));
+    let mut fn_instr_seq = fn_builder.func_body();
+
+    // copy instructions from fn_id to new function
+    clone_rec(
+        fn_local_function,
+        fn_local_function.block(fn_local_function.entry_block()),
+        &mut fn_instr_seq,
+    );
+    let fn_copy_id = fn_builder.finish(fn_local_function.args.clone(), &mut module.funcs);
+
+    // number of instructions in original/wrapper and copied function should be the same
+    assert_eq!(
+        module.funcs.get(fn_id).kind.unwrap_local().size(),
+        module.funcs.get(fn_copy_id).kind.unwrap_local().size()
     );
 
-    let function_body = &mut module
+    let locals = &mut module.locals;
+    // create new local params for wrapper function, old params are copied (see clone above) to new
+    // function
+    let local_vars: Vec<LocalId> = params.iter().map(|t| locals.add(*t)).collect();
+    let mut instr_seq = module
         .funcs
-        .iter_local_mut()
-        .filter(|(id, _)| id == &function_id)
-        .next()
-        .unwrap()
-        .1
+        .get_mut(fn_id)
+        .kind
+        .unwrap_local_mut()
         .builder_mut()
         .func_body();
 
-    let locs = &mut module.locals;
-    let local_vars: Vec<LocalId> = arg_types.iter().map(|t| locs.add(*t)).collect();
+    // remove all instructions from wrapper function (they are copied over to new function)
+    *instr_seq.instrs_mut() = vec![];
 
-    // save function args to local var
-    local_vars
-        .iter()
-        .zip(function_args)
-        .for_each(|(local_var, func_arg)| {
-            function_body
-                .local_get_at(0, func_arg)
-                .local_set_at(1, *local_var);
-        });
-    let return_val = match ret_types[..] {
-        [t] => {
-            // we assume there is only one return
-            Some(module.locals.add(t))
-        }
-        _ => None,
-    };
+    // prepare args to call new function
+    local_vars.iter().for_each(|l| {
+        instr_seq.local_get(*l);
+    });
 
-    // find return instruction indexes
-    let return_indexes: Vec<usize> = function_body
-        .instrs()
-        .iter()
-        .enumerate()
-        .filter(|(_, (instr, _))| match instr {
-            ir::Instr::Return(_) => true,
-            _ => false,
-        })
-        .map(|(i, _)| i)
-        .collect();
+    // call new copied function from wrapper function
+    instr_seq.call(fn_copy_id);
 
-    let end_index = function_body.instrs().len();
-    // Insert profiler function call at specific position
-    let mut insert_profiler_at = |pos: usize| {
-        let insert_local_args = |body: &mut InstrSeqBuilder| {
-            local_vars.iter().rev().for_each(|var| {
-                body.local_get_at(pos, *var);
-            })
-        };
-        match return_val {
-            None => {
-                function_body.call_at(pos, profiler);
-                insert_local_args(function_body);
-            }
-            Some(ret_val) => {
-                function_body.call_at(pos, profiler);
-                function_body.local_get_at(pos, ret_val);
-                insert_local_args(function_body);
-                function_body.local_tee_at(pos, ret_val);
-            }
-        }
-    };
-
-    // call profiler at the end of the function
-    insert_profiler_at(end_index);
-    // call profiler before every return instruction
-    return_indexes
-        .iter()
-        .rev()
-        .for_each(|i| insert_profiler_at(*i));
+    // modify wrapper function args
+    module.funcs.get_mut(fn_id).kind.unwrap_local_mut().args = local_vars;
     Ok(())
+}
+
+fn clone_rec(fn_loc: &LocalFunction, instrs: &ir::InstrSeq, instrs_clone: &mut InstrSeqBuilder) {
+    instrs.instrs.iter().for_each(|(i, _)| match i {
+        ir::Instr::Block(block) => {
+            let block_instrs = fn_loc.block(block.seq);
+            instrs_clone.block(block_instrs.ty, |block_clone| {
+                clone_rec(fn_loc, block_instrs, block_clone);
+            });
+        }
+        ir::Instr::IfElse(if_else) => {
+            let consequent_instrs = fn_loc.block(if_else.consequent);
+            instrs_clone.if_else(
+                consequent_instrs.ty,
+                |consequent_clone| {
+                    clone_rec(fn_loc, consequent_instrs, consequent_clone);
+                },
+                |alternative_clone| {
+                    clone_rec(fn_loc, fn_loc.block(if_else.alternative), alternative_clone);
+                },
+            );
+        }
+        ir::Instr::Loop(loop_) => {
+            let loop_instrs = fn_loc.block(loop_.seq);
+            instrs_clone.block(loop_instrs.ty, |loop_clone| {
+                clone_rec(fn_loc, loop_instrs, loop_clone);
+            });
+        }
+        _ => {
+            instrs_clone.instr(i.clone());
+        }
+    });
 }
